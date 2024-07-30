43218995e11a4868b5ae790db5607ff0397012ff
Peter Eliason <peter@petereliason.com>
2024-07-30 01:32:08 -0500

    added looper and changed ip

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const BulbWrapper = require('./bulb');
 const client = new Client();
 
 (async () => {
-  const device = await client.getDevice({ host: '192.168.254.35' })
+  const device = await client.getDevice({ host: '192.168.68.113' })
 
   const isOn = await device.getPowerState()
 
@@ -14,5 +14,6 @@ const client = new Client();
 
   const bulb = new BulbWrapper(device)
 
-  bulb.startDisco(350)
+  //bulb.startDisco(350)
+  bulb.startFlagLoop(5000)
 })()
